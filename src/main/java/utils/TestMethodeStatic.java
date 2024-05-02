3f2dbcfd4f45b08f57d729ce7f48e42f02ada426
Cherhazad <cherhazad.boustani@outlook.fr>
2024-05-02 23:25:53 +0200

    tp 4

---
+++ src/main/java/utils/TestMethodeStatic.java
@@ -0,0 +1,20 @@
+package utils;
+
+
+public class TestMethodeStatic {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	
+		String chaine = "12";
+		int valeur = Integer.parseInt(chaine);
+		System.out.println(valeur);
+		
+		int a = 15;
+		int b = 8;
+		
+		int max = Integer.max(a,b);
+	
+
+}
