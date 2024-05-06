c02b6696c9bd3df94df4a5f5d8e0442292205dec
Cherhazad <cherhazad.boustani@outlook.fr>
2024-05-07 00:45:51 +0200

    TP 1 autoformation - exo ManipulationChaine mis à jour

--- src/main/java/fr/diginamic/chaines/ManipulationChaine.java
+++ src/main/java/fr/diginamic/chaines/ManipulationChaine.java
@@ -10,31 +10,55 @@ public class ManipulationChaine {
 		// TODO Auto-generated method stub
 
 		String chaine = "Durand;Marcel;2 523.5";
+		
+		//1. méthode charAt(int index)
 
 		char premierCaractere = chaine.charAt(0);
 		System.out.println("Premier caractère: " + premierCaractere);
+		
+		//2. méthode length()
 
 		int longueurCaracteres = chaine.length();
 		System.out.println("Longueur chaine de caractères: " + longueurCaracteres);
 
+		
+		//3. méthode indexOf(char c)
 		char c = ';';
 		int rechercheIndex = chaine.indexOf(c);
 		System.out.println("Index de ce premier caractère " + " \"" + c + "\": " + " " + rechercheIndex);
 
-		String nomMaj = chaine.toUpperCase().substring(0, 6);
-		System.out.println(nomMaj);
+		
+		//4. méthode substring(int start, int end)
+		
+		String extraction = chaine.substring(0, chaine.indexOf(c));
+		System.out.println("extraction: " + extraction);
+		
+		
+		//5. méthode toUpperCase()
+		String nomMaj = chaine.toUpperCase().substring(0, chaine.indexOf(c));
+		System.out.println("Uppercase: " + nomMaj);
+		
+		//6. méthode toLowerCase()
 
-		String nomMin = chaine.toLowerCase().substring(0, 6);
-		System.out.println(nomMin);
-
-		String[] separate = chaine.split(";");
+		String nomMin = chaine.toLowerCase().substring(0, chaine.indexOf(c));
+		System.out.println("Lowercase: " + nomMin);
+		
+		//9.a. méthode replace() pour retrait espace
+		String chaineDbl = chaine.replace(" ", "");
+		System.out.println("Retrait espace: " + chaineDbl);
+		
+		//7. méthode split
+		String[] separate = chaineDbl.split(";");
 		System.out.println(Arrays.toString(separate));
+			
+		//9.b. conversion String en double
+		double salaire = Double.parseDouble(separate[2]);
+		//System.out.println("salaire " + salaire);
 		
-		double salaireDbl = Double.parseDouble(separate[2]);
 		
-		Salarie num1 = new Salarie();
-		num1.ajoutSalarie(separate);
-		System.out.println(num1.nom + " " + num1.prenom);
+		//9. Création d'instance de salarié
+		Salarie num1 = new Salarie(separate[0], separate[1], salaire);
+		System.out.println(num1.nom + " " + num1.prenom + " " + num1.salaire);
 
 	}
 
--- src/main/java/fr/diginamic/entites/Salarie.java
+++ src/main/java/fr/diginamic/entites/Salarie.java
@@ -5,17 +5,11 @@ public class Salarie {
 	public String nom;
 	public String prenom;
 	public double salaire;
-	
-	
-	public Salarie() {
-	
-	}
-	
-	
-	public void ajoutSalarie(String[] newSalarie) {
-		 this.nom = newSalarie[0];
-		 this.prenom = newSalarie[1];
-		 this.salaire = newSalarie[2];
+
+	public Salarie(String nom, String prenom, double salaire) { // String[] separate
+		this.nom = nom;
+		this.prenom = prenom;
+		this.salaire = salaire;
 	}
 
 }
