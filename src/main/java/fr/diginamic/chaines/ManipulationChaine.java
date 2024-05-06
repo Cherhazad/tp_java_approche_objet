afe97c6044b5e4395d46a5627bc64235c83567ef
Cherhazad <cherhazad.boustani@outlook.fr>
2024-05-06 17:33:50 +0200

    TP 1 autoformation - exo ManipulationChaine

--- src/main/java/fr/diginamic/chaines/ManipulationChaine.java
+++ src/main/java/fr/diginamic/chaines/ManipulationChaine.java
@@ -2,32 +2,40 @@ package fr.diginamic.chaines;
 
 import java.util.Arrays;
 
+import fr.diginamic.entites.Salarie;
+
 public class ManipulationChaine {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 		String chaine = "Durand;Marcel;2 523.5";
-		
+
 		char premierCaractere = chaine.charAt(0);
 		System.out.println("Premier caractère: " + premierCaractere);
-		
+
 		int longueurCaracteres = chaine.length();
 		System.out.println("Longueur chaine de caractères: " + longueurCaracteres);
-		
+
 		char c = ';';
 		int rechercheIndex = chaine.indexOf(c);
-		System.out.println("Index de ce premier caractère " + " \""  + c + "\": " + " " + rechercheIndex);
-		
+		System.out.println("Index de ce premier caractère " + " \"" + c + "\": " + " " + rechercheIndex);
+
 		String nomMaj = chaine.toUpperCase().substring(0, 6);
 		System.out.println(nomMaj);
-		
+
 		String nomMin = chaine.toLowerCase().substring(0, 6);
 		System.out.println(nomMin);
-		
+
 		String[] separate = chaine.split(";");
 		System.out.println(Arrays.toString(separate));
 		
+		double salaireDbl = Double.parseDouble(separate[2]);
+		
+		Salarie num1 = new Salarie();
+		num1.ajoutSalarie(separate);
+		System.out.println(num1.nom + " " + num1.prenom);
+
 	}
 
 }
--- src/main/java/fr/diginamic/entites/Salarie.java
+++ src/main/java/fr/diginamic/entites/Salarie.java
@@ -2,4 +2,20 @@ package fr.diginamic.entites;
 
 public class Salarie {
 
+	public String nom;
+	public String prenom;
+	public double salaire;
+	
+	
+	public Salarie() {
+	
+	}
+	
+	
+	public void ajoutSalarie(String[] newSalarie) {
+		 this.nom = newSalarie[0];
+		 this.prenom = newSalarie[1];
+		 this.salaire = newSalarie[2];
+	}
+
 }
