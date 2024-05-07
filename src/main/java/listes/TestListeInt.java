b85c1881c0802a1070e1aa96aad70cebd2f50374
Cherhazad <cherhazad.boustani@outlook.fr>
2024-05-07 14:19:30 +0200

    TP 10 - Exercice 3 - TestListeInt fini

--- src/main/java/listes/TestListeInt.java
+++ src/main/java/listes/TestListeInt.java
@@ -6,24 +6,23 @@ import java.util.Iterator;
 import java.util.List;
 
 public class TestListeInt {
-	
+
 	public static void main(String[] args) {
-	
+
 		List<Integer> array = new ArrayList<Integer>();
 		Collections.addAll(array, -1, 5, 7, 3, -2, 4, 8, 5);
-		
+
 		System.out.println(array);
 		System.out.println("Taille de la liste: " + array.size());
-		
+
 		// valeur max
 		int maxValue = Collections.max(array);
 		System.out.println("Valeur maximale: " + maxValue);
-		
-		//valeur min
+
+		// valeur min
 		int minValue = Collections.min(array);
 		System.out.println("Valeur minimale: " + minValue);
-		
-		
+
 		// supprimer la plus petite valeur
 		Iterator<Integer> iter = array.iterator();
 		while (iter.hasNext()) {
@@ -32,9 +31,17 @@ public class TestListeInt {
 				iter.remove();
 			}
 		}
-		
+
 		System.out.println(array);
-	
-		//conversion négatif au positif
+
+		// conversion négatif au positif
+
+		for (int i = 0; i < array.size(); i++) {
+			if (array.get(i) < 0) {
+				int negValue = Math.abs(array.get(i));
+				array.set(i, negValue);
+			}
+		}
+		System.out.println(array);
 	}
 }
