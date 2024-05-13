6faa94c5a7102ae569d4ef36ee0c43af91022333
Cherhazad <cherhazad.boustani@outlook.fr>
2024-05-13 16:54:49 +0200

    TP12 - exercice 2

--- src/main/java/fr/diginamic/maps/CreationEtManipulationMap.java
+++ src/main/java/fr/diginamic/maps/CreationEtManipulationMap.java
@@ -1,9 +1,30 @@
 package fr.diginamic.maps;
 
+import java.util.HashMap;
+
 public class CreationEtManipulationMap {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		HashMap<Integer, String> mapVilles = new HashMap<>();
+		mapVilles.put(13, "Marseille");
+		mapVilles.put(34, "Montpellier");
+		mapVilles.put(44, "Nantes");
+		mapVilles.put(75, "Paris");
+		mapVilles.put(31, "Toulouse");
+		mapVilles.put(59, "Lille");
+		mapVilles.put(69, "Lyon");
+		mapVilles.put(33, "Bordeaux");
+
+		for (Integer cle : mapVilles.keySet()) {
+			System.out.println(cle);
+		}
+		
+		for (String villes : mapVilles.values()) {
+			System.out.println(villes);
+		}
+		
+		System.out.println(mapVilles.size());
 
 	}
 
