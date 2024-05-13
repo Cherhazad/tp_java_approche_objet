4c6dd01aee0b488de27fd48a7b515d078f86da0a
Cherhazad <cherhazad.boustani@outlook.fr>
2024-05-13 10:30:55 +0200

    TP15 - exercice 2 CreerFichier

--- src/main/java/fr/diginamic/fichier/CreerFichier.java
+++ src/main/java/fr/diginamic/fichier/CreerFichier.java
@@ -4,16 +4,30 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.ArrayList;
 import java.util.List;
 
 public class CreerFichier {
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		
-		Path pathFile = Paths.get("/Users/plantsarelife/Desktop/Diginamic/Cours/11. Java/Approche POO/12. Java approche objet/TP/recensement.csv");
+
+		Path pathFile = Paths.get(
+				"/Users/plantsarelife/Desktop/Diginamic/Cours/11. Java/Approche POO/12. Java approche objet/TP/recensement.csv");
+
 		List<String> lignes = Files.readAllLines(pathFile);
-		//List<String> newLignes = lignes.substring(0, 100);
+		ArrayList<String> lignes100 = new ArrayList();
+		
+		
+		for (int i = 0; i < 100; i++) {
+			System.out.println(lignes.get(i));
+			lignes100.add(lignes.get(i));
+		}
+		
+		Path pathCible = Paths.get("/Users/plantsarelife/Desktop/Diginamic/Cours/11. Java/Approche POO/12. Java approche objet/TP/recensement2.csv");
+		Files.write(pathCible, lignes100);
+		
 	}
 
 }
